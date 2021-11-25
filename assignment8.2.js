b591e6692a180b1cf1ad7a74d0a5f8ec7b18a24d
affab007 <affab007@gmail.com>
2021-11-25 11:53:08 +0700

    counting the number of characters

---
+++ assignment8.2.js
@@ -0,0 +1,4 @@
+var inputName;
+inputName = prompt("What is the input string?","")
+var numberOfLetters = inputName.length;
+document.write(inputName ,"\n \'has\' \n",numberOfLetters,"\n \'characters.\'");
