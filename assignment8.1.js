ee68467ea4aa10e6297dcc0b497aa348f6e75efe
affab007 <affab007@gmail.com>
2021-11-25 11:52:02 +0700

    say 'hello'

---
+++ assignment8.1.js
@@ -0,0 +1,5 @@
+var inputName;
+inputName = prompt("what is your name","โปรดระบุชื่อ")
+var numberOfLetters = inputName.length;
+console.log("\'Hello\' \n",inputName, numberOfLetters);
+document.write("\'Hello\' \n", inputName ,"\n \'nice to meet you\' ");
