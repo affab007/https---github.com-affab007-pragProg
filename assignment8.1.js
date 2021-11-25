5fd7cc576606350a63a77b298107d15c5845087b
affab007 <affab007@gmail.com>
2021-11-25 12:10:39 +0700

    decorate

--- assignment8.1.js
+++ assignment8.1.js
@@ -2,4 +2,4 @@ var inputName;
 inputName = prompt("what is your name","โปรดระบุชื่อ")
 var numberOfLetters = inputName.length;
 console.log("\'Hello\' \n",inputName, numberOfLetters);
-document.write("\'Hello\' \n", inputName ,"\n \'nice to meet you\' ");
+document.write("\ Hello\ \n", inputName ,"\n \ nice to meet you\ ");
--- assignment8.2.js
+++ assignment8.2.js
@@ -1,4 +1,4 @@
 var inputName;
 inputName = prompt("What is the input string?","")
 var numberOfLetters = inputName.length;
-document.write(inputName ,"\n \'has\' \n",numberOfLetters,"\n \'characters.\'");
+document.write(inputName ,"\n \ has \ \n",numberOfLetters,"\n \ characters. \ ");
