8c5a1d898ca29ee161c843937bd4eef8bd2dab7a
affab007 <affab007@gmail.com>
2021-11-26 13:10:03 +0700

    Simple Math

---
+++ assignment12.js
@@ -0,0 +1,9 @@
+inputFirstNumber = parseInt(prompt("What is the first number?","Please enter a number"))
+inputSecondNumber = parseInt(prompt("What is the second number?","Please enter a number"))
+
+console.log("What is the first number?",inputFirstNumber,"\n",
+"What is the second number?",inputSecondNumber,"\n",
+inputFirstNumber," + ",inputSecondNumber," = ",(inputFirstNumber+inputSecondNumber),"\n",
+inputFirstNumber," - ",inputSecondNumber," = ",(inputFirstNumber-inputSecondNumber),"\n",
+inputFirstNumber," * ",inputSecondNumber," = ",(inputFirstNumber*inputSecondNumber),"\n",
+inputFirstNumber," / ",inputSecondNumber," = ",(inputFirstNumber/inputSecondNumber),"\n",)
