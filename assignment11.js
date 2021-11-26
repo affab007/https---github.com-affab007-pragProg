05156b9a6d1cd177e09593d266c440b4467c6aac
affab007 <affab007@gmail.com>
2021-11-26 13:09:45 +0700

    Mad Libs

---
+++ assignment11.js
@@ -0,0 +1,9 @@
+inputNoun = prompt("Enter a noun","Please specify")
+inputVerb = prompt("Enter a verb","Please specify")
+inputAdjective = prompt("Enter a adjective","Please specify")
+inputAdverb = prompt("Enter a adverb","Please specify")
+console.log("Enter a noun",inputNoun,"\n",
+"Enter a verb",inputVerb,"\n",
+"Enter a adjective",inputAdjective,"\n",
+"Enter a adverb",inputAdverb,"\n",
+"do you",inputVerb,"your",inputAdjective,inputNoun,inputAdverb,"\?"," นี่แหละที่มันฮา!")
