bdaeabb292bc8c08ae3809344d37dfdaf908386e
affab007 <affab007@gmail.com>
2021-11-29 20:53:09 +0700

    unit conversion

---
+++ assignment14.js
@@ -0,0 +1,11 @@
+translator = 0.09290304;
+lotrif = parseInt(prompt("What is the length of the room in feet?","Please enter a length"));
+wotrif = parseInt(prompt("What is the width of the room in feet?","Please enter a width"));  
+
+TheAreaIs = (lotrif*wotrif)
+calculate = (lotrif*wotrif*translator)
+
+console.log("What is the length of the room in feet?",lotrif,"\n",
+"What is the length of the room in feet?",wotrif,"\n",
+"You entered dimensions of ",lotrif," feet by ",wotrif," feet","\n",
+"The area is ",TheAreaIs," square feet",calculate.toFixed(3),"square meters");
