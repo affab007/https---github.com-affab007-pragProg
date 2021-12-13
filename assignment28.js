a5418f35288482b3ca0a0eae7fd49a84be08f125
affab007 <affab007@gmail.com>
2021-12-13 17:25:29 +0700

    converting numbers to text

---
+++ assignment28.js
@@ -0,0 +1,48 @@
+let numberOfTheMonth
+numberOfTheMonth = Number(prompt("Please enter the number of the month"));
+let month;
+switch (numberOfTheMonth) {
+     case 1:
+          month = "มกราคม";
+          break;
+     case 2:
+          month = "กุมภาพันธ์";
+          break;
+     case 3:
+          month = "มีนาคม";
+          break;
+     case 4:
+          month = "เมษายน";
+          break;
+     case 5:
+          month = "พฤษภาคม";
+          break;
+     case 6:
+          month = "มิถุนายน";
+          break;
+     case 7:
+          month = "กรกฏาคม";
+          break;
+     case 8:
+          month = "สิงหาคม";
+          break;
+     case 9:
+          month = "กันยายน";
+          break;
+     case 10:
+          month = "ตุลาคม";
+          break;
+     case 11:
+          month = "พฤศจิกายน";
+          break;
+     case 12:
+          month = "ธันวาคม";
+          break;
+     default:
+          month = "Please enter a new number.";
+          window.alert("Please enter the number of new orders.");
+          location.reload();
+          break;
+}
+console.log("Please enter the number of the month:", numberOfTheMonth, "\n",
+     "The name of the month is:", month); 
