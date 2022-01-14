1743a274d5f198d5a7f6626ce74f7557fc43ad85
affab007 <affab007@gmail.com>
2022-01-14 19:36:05 +0700

    Start Loop

---
+++ assignment37.js
@@ -0,0 +1,10 @@
+let result;
+
+for (let firstNum = 0; firstNum < 13;firstNum ++) {
+
+    for (let secondNum = 0; secondNum < 13;secondNum ++){
+
+    result = firstNum*secondNum;
+    console.log(firstNum+" x "+secondNum+" = "+result+"\n")
+    }
+    console.log("\n"+"\n")}
